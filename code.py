6bbf1e742061b2d54f36d5283a06256e670f09fb
Mihailo Lalevic <mlalevic@gmail.com>
2013-10-13 19:00:01 +0100

    Added wsgi handler

--- code.py
+++ code.py
@@ -1,1 +1,1 @@
-import web  urls = (     '/', 'index' )  class index:     def GET(self):         return "Hello, world!"  if __name__ == "__main__":     app = web.application(urls, globals())     app.run()
+import web  urls = (     '/', 'index' )  class index:     def GET(self):         return "Hello, world!" def wsgiHandler():     return web.application(urls, globals(), autoreload=False).wsgifunc() if __name__ == "__main__":     app = web.application(urls, globals())     app.run()
